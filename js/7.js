22739b14125c97c7c4a839ee31fe5c0e099cca1a
kinoko2k <inokoryotarou@gmail.com>
2023-01-30 23:10:57 +0900

    add

--- js/7.js
+++ js/7.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
 
@@ -12,19 +11,4 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-label", "close menu");
     this.setAttribute("aria-expanded", "true");
   }
-=======
-let navToggle = document.querySelector(".nav__toggle");
-let navWrapper = document.querySelector(".nav__wrapper");
-
-navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
-    this.setAttribute("aria-expanded", "false");
-    this.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
-  } else {
-    navWrapper.classList.add("active");
-    this.setAttribute("aria-label", "close menu");
-    this.setAttribute("aria-expanded", "true");
-  }
->>>>>>> a40619603bdd0a6245f5bf430df8eeb8fa424e8a
 });
